79d2b58d2f61e2a784a672c1d42b40e04e6fe565
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-05-31 00:27:34 +0200

    add shared to theme module

--- src/app/core/features/theme/theme.module.ts
+++ src/app/core/features/theme/theme.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [ThemeToggleComponent],
+  imports: [CommonModule, SharedModule]
 })
 export class ThemeModule {}
