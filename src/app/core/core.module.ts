7737c422a11035bfd7c5bffe6e4ab073e985f59e
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-05-31 23:34:11 +0200

    add components to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './layout/home/home.component';
-import { HomeHeaderComponent } from './layout/home-header/home-header.component';
 import { HomeLeftbarComponent } from './layout/home-leftbar/home-leftbar.component';
 import { HomeLogoComponent } from './layout/home-logo/home-logo.component';
 import { HomeNavigationComponent } from './layout/home-navigation/home-navigation.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ThemeModule } from './features/theme/theme.module';
+import { HomeMainContentComponent } from './layout/home-main-content/home-main-content.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HomeHeaderComponent,
     HomeLeftbarComponent,
     HomeLogoComponent,
-    HomeNavigationComponent
+    HomeNavigationComponent,
+    HomeMainContentComponent
   ],
   imports: [CommonModule, SharedModule, ThemeModule],
   exports: [HomeNavigationComponent]
