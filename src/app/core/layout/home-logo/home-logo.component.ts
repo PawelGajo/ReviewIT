a84536cde8e58e717253de5a4d0df04ad6edf77d
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-05-31 23:30:31 +0200

    add logo

---
+++ src/app/core/layout/home-logo/home-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-logo',
+  template: ` <p>home-logo works!</p> `,
+  styles: []
+})
+export class HomeLogoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
