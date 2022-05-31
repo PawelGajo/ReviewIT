4f9406506f70a2f710e071c78e5ebbe46027cd4e
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-05-31 16:38:10 +0200

    add formsmodule to shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatSlideToggleModule],
-  exports: [MatSlideToggleModule]
+  imports: [CommonModule, MatSlideToggleModule, FormsModule],
+  exports: [MatSlideToggleModule, FormsModule]
 })
 export class SharedModule {}
