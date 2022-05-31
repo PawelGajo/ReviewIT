4da39356149971ce45252f4eedf7c9f1066811a9
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-05-31 22:33:27 +0200

    add home navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './layout/home/home.component';
+import { HomeHeaderComponent } from './layout/home-header/home-header.component';
+import { HomeLeftbarComponent } from './layout/home-leftbar/home-leftbar.component';
+import { HomeLogoComponent } from './layout/home-logo/home-logo.component';
+import { HomeNavigationComponent } from './layout/home-navigation/home-navigation.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ThemeModule } from './features/theme/theme.module';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, ThemeModule]
+  declarations: [
+    HomeComponent,
+    HomeHeaderComponent,
+    HomeLeftbarComponent,
+    HomeLogoComponent,
+    HomeNavigationComponent
+  ],
+  imports: [CommonModule, SharedModule, ThemeModule],
+  exports: [HomeNavigationComponent]
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatSlideToggleModule, FormsModule],
-  exports: [MatSlideToggleModule, FormsModule]
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    MatSlideToggleModule,
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
 })
 export class SharedModule {}
