1a9ecf4d5e3106feabc5601cc2de3c0fd17937b0
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-05-31 22:34:50 +0200

    dd routing to homeNav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
+import { HomeNavigationComponent } from './core/layout/home-navigation/home-navigation.component';
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeNavigationComponent,
+    path: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
