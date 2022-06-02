2c84900750cd6da09f89939c1d3ca6efedb2b639
pawel.gajo@snt.pl <pawel.gajo@snt.pl>
2022-06-02 22:48:33 +0200

    add input post

--- src/app/core/features/post/components/post-list-item/post-list-item.component.ts
+++ src/app/core/features/post/components/post-list-item/post-list-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PostListItem } from '../../models/Post';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
   styleUrls: ['./post-list-item.component.scss']
 })
-export class PostListItemComponent implements OnInit {
+export class PostListItemComponent {
+  @Input() post: PostListItem;
   constructor() {}
-
-  ngOnInit(): void {}
 }
