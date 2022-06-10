693deb4ec80240b5af2979e2720e26729fc3a5d5
=PawelGajo <pawel.gajo@snt.pl>
2022-06-11 00:34:20 +0200

    test load and search posts

--- src/app/core/features/post/components/posts-container/posts-container.component.spec.ts
+++ src/app/core/features/post/components/posts-container/posts-container.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick
+} from '@angular/core/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { MOCK_POST_LIST_ITEMS } from '../../models/mock-post-list';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
@@ -10,6 +15,7 @@ describe('PostsContainerComponent', () => {
   let component: PostsContainerComponent;
   let fixture: ComponentFixture<PostsContainerComponent>;
   let posts: PostListItem[] = MOCK_POST_LIST_ITEMS;
+  let store: MockStore;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PostsContainerComponent],
@@ -26,7 +32,7 @@ describe('PostsContainerComponent', () => {
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
 
-    TestBed.inject(MockStore);
+    store = TestBed.inject(MockStore);
   });
 
   beforeEach(() => {
@@ -38,4 +44,32 @@ describe('PostsContainerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get posts from store on init', fakeAsync(() => {
+    let fetchedPosts: PostListItem[] | undefined;
+    component.posts$.subscribe({
+      next: (posts) => (fetchedPosts = posts),
+      error: () => fail('Should be no error'),
+      complete: () => fail('Should be no complete')
+    });
+    const storeDispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    component.ngOnInit();
+    tick();
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(1);
+    expect(fetchedPosts).toEqual(posts);
+  }));
+
+  it('should get queried posts from store on search fn', fakeAsync(() => {
+    let fetchedPosts: PostListItem[] | undefined;
+    component.posts$.subscribe({
+      next: (posts) => (fetchedPosts = posts),
+      error: () => fail('Should be no error'),
+      complete: () => fail('Should be no complete')
+    });
+    const storeDispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    component.search('test');
+    tick();
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(1);
+    expect(fetchedPosts).toEqual(posts);
+  }));
 });
